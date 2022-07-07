2abc49695b9c2fa9768de52c9ec956b9574c4b04
Saran B M <saran.b.m.001@gmail.com>
2022-07-07 23:49:26 +0530

    Organize js code

--- js/script.js
+++ js/script.js
@@ -6,67 +6,72 @@ let gameBoard = (function (){
     let tiles = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
     let won = -1;
     let player = 1;
+
+    const getTiles = () => tiles;
+    const getWon = () => won;
+    const setWon = (x) => won = x;
+    const getPlayer = () => player;
     let markTile = (tile) => {
         if(tiles[tile]===-1){
-            if(gameBoard.player === 1){
+            if(player === 1){
                 tiles[tile] = 1;
             }
             else {
                 tiles[tile] = 0;
             }
             
-            gameBoard.player = 1-gameBoard.player;
+            player = 1-player;
             
         }
     }
     let checkGameStatus = () => {
         if(tiles[0]===1 && tiles[1]===1 && tiles[2]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[0]===0 && tiles[1]===0 && tiles[2]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[0]===1 && tiles[3]===1 && tiles[6]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[0]===0 && tiles[3]===0 && tiles[6]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[0]===1 && tiles[4]===1 && tiles[8]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[0]===0 && tiles[4]===0 && tiles[8]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[8]===1 && tiles[5]===1 && tiles[2]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[8]===0 && tiles[5]===0 && tiles[2]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[8]===1 && tiles[7]===1 && tiles[6]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[8]===0 && tiles[7]===0 && tiles[6]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[1]===1 && tiles[4]===1 && tiles[7]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[1]===0 && tiles[4]===0 && tiles[7]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[3]===1 && tiles[4]===1 && tiles[5]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[3]===0 && tiles[4]===0 && tiles[5]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         else if(tiles[6]===1 && tiles[4]===1 && tiles[2]===1){
-            gameBoard.won = 1;
+            won = 1;
         }
         else if(tiles[6]===0 && tiles[4]===0 && tiles[2]===0){
-            gameBoard.won = 0;
+            won = 0;
         }
         let draw=true;
         for(let tile of tiles){
@@ -78,33 +83,35 @@ let gameBoard = (function (){
         }
         if(draw) return 'Draw';
         
-        if(gameBoard.won === 1) return 'X Won';
-        else if(gameBoard.won === 0) return 'O Won';
+        if(won === 1) return 'X Won';
+        else if(won === 0) return 'O Won';
         else return
 
     }
 
     let resetGame = () => {
         tiles = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
-        gameBoard.tiles = tiles;
-        gameBoard.won = -1;
+        tiles = tiles;
+        won = -1;
         
-        gameBoard.player = 1;
+        player = 1;
     }
 
 
-    return {tiles, player, won,  markTile, resetGame, checkGameStatus};
+    return {getTiles, getWon,setWon ,getPlayer, markTile, resetGame, checkGameStatus};
 })();
 
 //create an object using module called displayController that has the function to 
 //show the layout like the game board whose turn to play and
 //play
 let displayController = (function (){
+
     let replay = document.querySelector("button");
     let tiles = Array.from(document.querySelectorAll(".tile"));
-    let player = document.querySelector("h1");
+    let player = document.querySelector(".statusBoard>h1");
     let gameLayout = document.querySelector(".gameLayout");
     let gameOverDiv = document.querySelector(".gameOver");
+
     let addEventListeners = function(){
         for(let tile of tiles){
             tile.addEventListener("click", markTile);
@@ -123,12 +130,12 @@ let displayController = (function (){
         
         for(let i=0; i<tiles.length; i++){
             let tileContent;
-            if(gameBoard.tiles[i]===1) tileContent="X";
-            else if(gameBoard.tiles[i]===0) tileContent="O";
+            if(gameBoard.getTiles()[i]===1) tileContent="X";
+            else if(gameBoard.getTiles()[i]===0) tileContent="O";
             else tileContent = " ";
             tiles[i].textContent = tileContent;
         }
-        player.textContent = `Turn of ${gameBoard.player?'X':'O'}`;
+        player.textContent = `Turn of ${gameBoard.getPlayer()?'X':'O'}`;
     }
 
     let resetGame = () => {
